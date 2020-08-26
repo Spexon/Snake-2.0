ca8e4e2364efef37033cae18484718986e7fcca0
Vladimir <vvhardy0143@eagle.fgcu.edu>
2020-08-26 14:04:58 -0400

    Added apple generation and score

--- src/sample/GameController.java
+++ src/sample/GameController.java
@@ -2,28 +2,34 @@ package sample;
 
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 
 public class GameController implements Initializable {
 
-    @FXML
-    private Label deadLabel;
-    @FXML
-    private Label showDifficulty;
-    @FXML
-    private ImageView snakeHead;
-    @FXML
-    private ImageView snakeBody;
+
+    @FXML private Label scoreDisplayAnimation;
+    @FXML private Label scoreDisplay;
+    @FXML private Label deadLabel;
+    @FXML private Label showDifficulty;
+    @FXML private ImageView snakeHead;
+    @FXML private ImageView snakeBody;
+    @FXML private ImageView apple;
+    private int score = 0;
     private double bodyTurnLocationX;
     private double bodyTurnLocationY;
     private final Image snakeHeadRight = new Image(new FileInputStream("C:\\Users\\Vladimir\\OneDrive - Florida Gulf Coast University\\Personal Projects\\Snake 2.0\\src\\sample\\Snake_Head_Right.png"));
@@ -54,28 +60,29 @@ public class GameController implements Initializable {
                     snakeHead.setImage(snakeHeadRight);
                     snakeHead.setX(PreGameController.snake.getxPos());
                     moveBody();
-                    //snakeBody.setY(bodyTurnLocationY);
-                    //snakeBody.setX(PreGameController.snake.getxPos() - 50);
+                    snakeBody.setY(bodyTurnLocationY);
+                    snakeBody.setX(PreGameController.snake.getxPos() - 50);
                 } else if (PreGameController.snake.getDirectionFacing().equals("LEFT")) {
                     snakeHead.setImage(snakeHeadLeft);
                     snakeHead.setX(PreGameController.snake.getxPos());
                     moveBody();
-                    //snakeBody.setY(bodyTurnLocationY);
-                    //snakeBody.setX(PreGameController.snake.getxPos() + 50);
+                    snakeBody.setY(bodyTurnLocationY);
+                    snakeBody.setX(PreGameController.snake.getxPos() + 50);
                 } else if (PreGameController.snake.getDirectionFacing().equals("UP")) {
                     snakeHead.setImage(snakeHeadUp);
                     snakeHead.setY(PreGameController.snake.getyPos());
                     moveBody();
-                    //snakeBody.setX(bodyTurnLocationX);
-                    //snakeBody.setY(PreGameController.snake.getyPos() + 50);
+                    snakeBody.setX(bodyTurnLocationX);
+                    snakeBody.setY(PreGameController.snake.getyPos() + 50);
                 } else if (PreGameController.snake.getDirectionFacing().equals("DOWN")) {
                     snakeHead.setImage(snakeHeadDown);
                     snakeHead.setY(PreGameController.snake.getyPos());
                     moveBody();
-                    //snakeBody.setX(bodyTurnLocationX);
-                    //snakeBody.setY(PreGameController.snake.getyPos() - 50);
+                    snakeBody.setX(bodyTurnLocationX);
+                    snakeBody.setY(PreGameController.snake.getyPos() - 50);
                 }
                 notDead = PreGameController.snake.boundaryControl();
+                checkSnakeCollision();
                 if (!notDead) {
                     deadLabel.setVisible(true);
                 }
@@ -103,6 +110,53 @@ public class GameController implements Initializable {
     }
 
     /**
+     * Generates an apple and places it somewhere randomly on the screen
+     */
+    public void generateApple() {
+        Random rand = new Random();
+        apple.setX(rand.nextInt(1650) + 100);
+        apple.setY(rand.nextInt(850) + 100);
+    }
+
+    /**
+     * Checks if the snake hits an apple between certain parameters (the hitbox of the apple) or itself.
+     */
+    public void checkSnakeCollision() {
+
+        if (((snakeHead.getX() > apple.getX() - 50) && (snakeHead.getX() < apple.getX() + 50)) &&
+                ((snakeHead.getY() > apple.getY() - 50) && (snakeHead.getY() < apple.getY() + 50))) {
+            setScore();
+            generateApple();
+        }
+    }
+
+    /**
+     * Assigns the score at the top, and animates the score across the screen once an apple is eaten
+     */
+    public void setScore() {
+        score += 100;
+        scoreDisplay.setText("Score: " + score);
+        scoreDisplayAnimation.setVisible(true);
+
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setDuration(Duration.seconds(1));
+        translateTransition.setNode(scoreDisplayAnimation);
+        translateTransition.setFromX(apple.getX());
+        translateTransition.setFromY(apple.getY() - 100);
+        translateTransition.setToY(apple.getY() - 200);
+
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.seconds(1.2));
+        fadeTransition.setNode(scoreDisplayAnimation);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+
+        translateTransition.play();
+        fadeTransition.play();
+
+    }
+
+    /**
      * Sets up difficulty and other objects as program starts. Everything done here should be possible to do in FXML
      */
     @Override
@@ -110,5 +164,6 @@ public class GameController implements Initializable {
 
         showDifficulty.setText("Difficulty: " + PreGameController.snake.getDifficulty());
         gameLoop();
+        generateApple();
     }
 }
